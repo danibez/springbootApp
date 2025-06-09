7b89df09dad57dcbc8e0b847ced55095225404f6
Ewayrton Brito de Oliveira <ewayrtonbrito@gmail.com>
2025-06-08 23:49:14 -0300

    limpando código do DTO

--- src/main/java/com/example/demo/dto/LoginDTO.java
+++ src/main/java/com/example/demo/dto/LoginDTO.java
@@ -1,31 +1,21 @@
 package com.example.demo.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 /**
  * Data Transfer Object (DTO) para receber as credenciais de login (email e senha).
  * Esta é a versão tradicional usando 'class', compatível com todas as versões do Java.
  */
-public class LoginDTO {
+@Getter
+@AllArgsConstructor
 
+public class LoginDTO {
     // 1. Campos (fields) privados para encapsular os dados
     private final String email;
     private final String password;
 
-    // 2. Construtor público para inicializar os campos
-    public LoginDTO(String email, String password) {
-        this.email = email;
-        this.password = password;
-    }
-
-    // 3. Métodos "getters" públicos para permitir o acesso aos campos
-    public String getEmail() {
-        return email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
     // Setters não são incluídos para manter o objeto imutável (mais seguro).
-    // Métodos equals(), hashCode() e toString() são omitidos por simplicidade,
-    // mas podem ser gerados automaticamente pela sua IDE se necessário.
 }
